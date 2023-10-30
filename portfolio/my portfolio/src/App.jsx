b3b422536840911ea8dad340aed9b6c7c953e519
user nameQtadaAlBalwegit config --global user.name user <qtadaahmad@gmail.com>
2023-10-31 00:51:11 +0300

    port-stage-2

---
+++ portfolio/my portfolio/src/App.jsx
@@ -0,0 +1,15 @@
+import  Navbar  from "./Navbar/Navbar";
+import About from "./About/About";
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <Navbar />
+     <About />
+    </>
+  )
+}
+
+export default App
